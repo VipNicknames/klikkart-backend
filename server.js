e91e0d78a337156998676b25b303d571141c1fc4
Shri <anandfabricator216@gmail.com>
2026-05-09 14:51:31 +0530

    Replace products.json with in-code products array
    Replaced products.json with an in-code products array and updated the root route to serve index.html.

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require("express");
 const cors = require("cors");
-const products = require("./products.json");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.static(__dirname));
 
+const products = [
+  {
+    name: "Gaming Mouse",
+    price: "₹999",
+    image: "https://i.imgur.com/8Km9tLL.jpg"
+  },
+  {
+    name: "Gaming Keyboard",
+    price: "₹1999",
+    image: "https://i.imgur.com/27ZAHzq.jpg"
+  },
+  {
+    name: "Gaming Headphone",
+    price: "₹1499",
+    image: "https://i.imgur.com/2DsA49b.jpg"
+  }
+];
+
 app.get("/products", (req, res) => {
   res.json(products);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
